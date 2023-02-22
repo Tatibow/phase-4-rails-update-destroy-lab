19a5097587a0977138e5e96b47a25dca111098e0
Tatiana <Bowmantatiana2@gmail.com>
2023-02-22 05:28:30 -0800

    commit tree 23ba62b384c8f38bb5323d644c6a83fb68ac1147

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,9 +18,32 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+ def update
+  plant = Plant.find(params[:id])
+  plant.update(plant_params)
+  if(plant)
+    render json: plant, status: :ok
+  else
+ render_error_response
+  end
+ end
+
+ def destroy
+   plant = Plant.find(params[:id])
+   if plant
+    plant.destroy
+   else
+    render_error_response
+   end
+ end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  def render_error_response
+    render json: {error: "plant not found"}, status: :not_found
   end
 end
